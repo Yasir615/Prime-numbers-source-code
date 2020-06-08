1ff2238dea1bacf3986e01959bbf2677eb8dd219
Yasir Ali <Yasir128.ali@gmail.com>
2020-06-08 03:03:52 -0700

    argv

---
+++ hey.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+    int a,b,c,d,e,sum=0;
+    a=atoi(argv[1]);
+    b=atoi(argv[2]);
+    for(c=a;c<b;c++)
+    {
+        e=0;
+        for(d=2;d<c;d++)
+        {
+            if(c%d==0)
+            {
+                e=1;
+                break;
+            }
+        }
+        if(e==0 && c!=1)
+        {
+            sum=sum+c;
+        }
+    }
+    printf("%d",sum);
+}
+
+
