debe3baa41f49221943f265676158a49b5a0f01c
Yasir Ali <Yasir128.ali@gmail.com>
2020-06-15 02:01:39 -0700

    add heart pattern code in c

---
+++ star_heart_pattern.c
@@ -0,0 +1,231 @@
+/*Hey guys,i know its a long code to to print heart shape but if you know a little bit of for loop,if else,nested if else,unary operators,binary operators.
+Then,you can easily understand this code. */
+
+#include<stdio.h>
+int main()
+{
+    int a;
+    for(a=1;a<=17;a++)
+    {
+        if((a<=4) || (a==8) || (a==9) || (a==10))
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=13)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=3 || a==9)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=14)
+            {
+            printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=2)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=15)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a==1)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=16)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+     for(a=1;a<=17;a++)
+    {
+        if(a<=17)
+        {
+            printf("*");
+        }
+    }
+    printf("\n");
+     for(a=1;a<=17;a++)
+    {
+        if(a<=17)
+        {
+            printf("*");
+        }
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=17)
+        {
+            printf("*");
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=17)
+        {
+            printf("*");
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a==1)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=16)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=2)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=15)
+            {
+                printf("*");
+            }
+        }
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=3)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=14)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=4)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=13)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=5)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=12)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=6)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=11)
+            {
+                printf("*");
+            }
+        }
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=7)
+        {
+            printf(" ");
+        }
+        else
+        {
+            if(a<=10)
+            {
+                printf("*");
+            }
+        }
+
+    }
+    printf("\n");
+    for(a=1;a<=17;a++)
+    {
+        if(a<=8)
+        {
+        printf(" ");
+        }
+        else
+        {
+            if(a<=9)
+            {
+                printf("*");
+            }
+        }
+
+    }
+}
